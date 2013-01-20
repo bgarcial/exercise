dce32d75f4befe3b1ea50ba1ca7837cf4a56cc7e
Bernardo Augusto Garcia <botibagl@gmail.com>
2013-01-20 01:30:00 -0500

    Update status.js

--- status.js
+++ status.js
@@ -1,29 +1,28 @@
 
-
 var passwd_account = "12345";
 var name="Bernardo Garcia";
 var balance=2500000;
 var date = new Date();
 
 function card(){
-    var card = confirm("¿Deseas ingresar tu tarjeta?");
-		  if(card == true){
-      var passwd_enter = prompt("Ingresa por favor tu contraseña. Recuerda que solo tienes 3 intentos  ");
-        if (passwd_enter==passwd_account){
-          window.location="bienvenido.html";
-        }else{
-          for (var count = 0; count<=2; count++) {
-            prompt("Ingresa de nuevo tu contraseña");
-             if (count == 2){
-              alert("Tu tarjeta ha sido bloqueada por seguridad")
-                window.location="banco.html";
-             }       
-        }
-      }
-    }else{
-      window.location="banco.html";
-    }
-	}
+	var card = confirm("¿Deseas ingresar tu tarjeta?");
+ 	if(card == true){
+      		var passwd_enter = prompt("Ingresa por favor tu contraseña. Recuerda que solo tienes 3 intentos  ");
+        	if (passwd_enter==passwd_account){
+          		window.location="bienvenido.html";
+        	}else{
+          		for (var count = 0; count<=2; count++) {
+            		prompt("Ingresa de nuevo tu contraseña");
+             				if (count == 2){
+              				alert("Tu tarjeta ha sido bloqueada por seguridad")
+                			window.location="banco.html";
+             			}       
+        		}
+      		}
+    	}else{
+      		window.location="banco.html";
+    	}
+}
 
 function myBalance(balance,retiro){
 	//if(retiro == 0){
